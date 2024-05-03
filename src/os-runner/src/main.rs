4965db990f03812c6d1f9e14f3f07b1c2294d665
pantae <pantae35872@gmail.com>
2024-05-03 19:10:50 +0700

    git is so stupid

---
+++ src/os-runner/src/main.rs
@@ -0,0 +1,30 @@
+use std::{
+    env,
+    process::{Command, Stdio},
+};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let curr_dir = env::current_dir().expect("no current dir");
+    if let Some(test_kernel) = args.get(1) {
+        let root_dir = curr_dir.parent().unwrap().parent().unwrap();
+        let mut mv = Command::new("mv");
+        let mut makefile_build = Command::new("make");
+        let mut makefile_run = Command::new("make");
+        mv.arg(test_kernel)
+            .arg(root_dir.join("kernel.bin"))
+            .current_dir(&curr_dir);
+        makefile_build
+            .arg("maker-no-kernel")
+            .current_dir(&root_dir)
+            .stdout(Stdio::null())
+            .stderr(Stdio::null());
+        makefile_run
+            .arg("test-run")
+            .current_dir(&root_dir)
+            .stderr(Stdio::null());
+        mv.status().expect("Could not move");
+        makefile_build.status().expect("make file failed");
+        makefile_run.status().expect("run failed");
+    }
+}
