06e739201c623a8d5cae602d5f9fe868f9278a35
Pantae <pantae35872@gmail.com>
2024-10-08 00:46:30 +0700

    Better ring buffer it now 0.5% chance of failing

--- src/kernel/src/utils/circular_ring_buffer.rs
+++ src/kernel/src/utils/circular_ring_buffer.rs
@@ -1,10 +1,9 @@
-use core::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
+use core::sync::atomic::{AtomicUsize, Ordering};
 
 pub struct CircularRingBuffer<T, const N: usize> {
     buffer: [Option<T>; N],
     head: AtomicUsize,
     tail: AtomicUsize,
-    overflowed: AtomicBool,
 }
 
 impl<T, const N: usize> CircularRingBuffer<T, N> {
@@ -13,67 +12,36 @@ impl<T, const N: usize> CircularRingBuffer<T, N> {
             buffer: [const { None }; N],
             head: AtomicUsize::new(0),
             tail: AtomicUsize::new(0),
-            overflowed: AtomicBool::new(false),
         }
     }
 
     pub fn write(&self, value: T) {
-        let mut increased_tail = false;
-        match self
-            .overflowed
-            .compare_exchange(true, false, Ordering::Acquire, Ordering::Relaxed)
-        {
-            Ok(..) => {
-                let mut tail = self.tail.load(Ordering::Acquire);
-                let mut new_tail;
-                loop {
-                    new_tail = (tail + 1) % N;
-                    match self.tail.compare_exchange(
-                        tail,
-                        new_tail,
-                        Ordering::Acquire,
-                        Ordering::Relaxed,
-                    ) {
-                        Ok(_) => break,
-                        Err(updated) => tail = updated,
-                    }
-                }
-                increased_tail = true;
-            }
-            Err(..) => {}
-        };
-
         let mut head = self.head.load(Ordering::Acquire);
         let mut new_head;
-        let mut overflowed;
         loop {
             new_head = (head + 1) % N;
-            overflowed = new_head == 0;
-
-            // If the overflow is set and increase_tail is also set it will be invalid state
-            // because you cannot overflow in two write
-            if !(overflowed && increased_tail) {
-                match self.overflowed.compare_exchange(
-                    false,
-                    overflowed,
-                    Ordering::Acquire,
-                    Ordering::Relaxed,
-                ) {
-                    Ok(..) => {}
-                    Err(..) => {
-                        if overflowed {
-                            head = self.head.load(Ordering::Acquire);
-                            continue;
-                        }
-                    }
-                }
-            }
 
             match self
                 .head
                 .compare_exchange(head, new_head, Ordering::Acquire, Ordering::Relaxed)
             {
                 Ok(head) => {
+                    if head == 0 {
+                        let mut tail = self.tail.load(Ordering::Acquire);
+                        let mut new_tail;
+                        loop {
+                            new_tail = (tail + 1) % N;
+                            match self.tail.compare_exchange(
+                                tail,
+                                new_tail,
+                                Ordering::Acquire,
+                                Ordering::Relaxed,
+                            ) {
+                                Ok(_) => break,
+                                Err(updated) => tail = updated,
+                            }
+                        }
+                    }
                     return unsafe {
                         (*(self as *const _ as *mut CircularRingBuffer<T, N>)).buffer[head] =
                             Some(value);
@@ -88,8 +56,17 @@ impl<T, const N: usize> CircularRingBuffer<T, N> {
         let mut tail = self.tail.load(Ordering::Acquire);
         let mut new_tail;
         loop {
-            new_tail = if self.buffer[tail].is_some() {
-                (tail + 1) % N
+            new_tail = if self.buffer[match tail.checked_sub(1) {
+                Some(tail) => tail,
+                None => return None,
+            }]
+            .is_some()
+            {
+                let mut result = (tail + 1) % (N + 1);
+                if result == 0 {
+                    result = 1;
+                }
+                result
             } else {
                 tail
             };
@@ -102,8 +79,13 @@ impl<T, const N: usize> CircularRingBuffer<T, N> {
                 Ok(tail) => {
                     if tail != new_tail {
                         return unsafe {
-                            (*(self as *const _ as *mut CircularRingBuffer<T, N>)).buffer[tail]
-                                .take()
+                            (*(self as *const _ as *mut CircularRingBuffer<T, N>)).buffer[match tail
+                                .checked_sub(1)
+                            {
+                                Some(tail) => tail,
+                                None => return None,
+                            }]
+                            .take()
                         };
                     }
                     return None;
