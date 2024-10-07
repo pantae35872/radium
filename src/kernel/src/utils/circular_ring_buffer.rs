ab2457694b35de05bcb615c9dd29e08cdaba4af0
Pantae <pantae35872@gmail.com>
2024-10-08 02:24:12 +0700

    BETTER RING BUFFER

--- src/kernel/src/utils/circular_ring_buffer.rs
+++ src/kernel/src/utils/circular_ring_buffer.rs
@@ -1,15 +1,20 @@
-use core::sync::atomic::{AtomicUsize, Ordering};
+use core::{
+    cell::UnsafeCell,
+    sync::atomic::{AtomicUsize, Ordering},
+};
 
 pub struct CircularRingBuffer<T, const N: usize> {
-    buffer: [Option<T>; N],
+    buffer: [Slot<T>; N],
     head: AtomicUsize,
     tail: AtomicUsize,
 }
 
+unsafe impl<T, const N: usize> Sync for CircularRingBuffer<T, N> {}
+
 impl<T, const N: usize> CircularRingBuffer<T, N> {
     pub const fn new() -> Self {
         Self {
-            buffer: [const { None }; N],
+            buffer: [const { Slot::new() }; N],
             head: AtomicUsize::new(0),
             tail: AtomicUsize::new(0),
         }
@@ -42,10 +47,8 @@ impl<T, const N: usize> CircularRingBuffer<T, N> {
                             }
                         }
                     }
-                    return unsafe {
-                        (*(self as *const _ as *mut CircularRingBuffer<T, N>)).buffer[head] =
-                            Some(value);
-                    };
+                    self.buffer[head].write(value);
+                    break;
                 }
                 Err(updated) => head = updated,
             };
@@ -93,5 +96,88 @@ impl<T, const N: usize> CircularRingBuffer<T, N> {
                 Err(updated) => tail = updated,
             }
         }
+    }
+}
+
+struct Slot<T> {
+    state: AtomicUsize, // 0 = empty, 1 = writing, 2 = full
+    data: UnsafeCell<Option<T>>,
+}
+
+impl<T> Slot<T> {
+    const fn new() -> Self {
+        Self {
+            state: AtomicUsize::new(0),
+            data: UnsafeCell::new(None),
+        }
+    }
+
+    fn write(&self, value: T) {
+        loop {
+            match self
+                .state
+                .compare_exchange(0, 1, Ordering::Acquire, Ordering::Relaxed)
+            {
+                Ok(..) => {
+                    let data = unsafe { &mut *self.data.get() };
+                    *data = Some(value);
+
+                    self.state.store(2, Ordering::Release);
+                    break;
+                }
+                Err(state) => match state {
+                    1 => continue,
+                    2 => {
+                        let data = unsafe { &mut *self.data.get() };
+                        *data = Some(value);
+                        break;
+                    }
+                    _ => panic!("Invalid state in slot"),
+                },
+            }
+        }
+    }
+
+    fn take(&self) -> Option<T> {
+        loop {
+            match self
+                .state
+                .compare_exchange(2, 1, Ordering::Acquire, Ordering::Relaxed)
+            {
+                Ok(..) => {
+                    let data = unsafe { &mut *self.data.get() };
+                    let result = data.take();
+
+                    self.state.store(0, Ordering::Release);
+
+                    return result;
+                }
+                Err(state) => match state {
+                    1 => continue,
+                    0 => break,
+                    _ => panic!("Invalid state in slot"),
+                },
+            }
+        }
+        return None;
+    }
+
+    fn is_some(&self) -> bool {
+        loop {
+            match self
+                .state
+                .compare_exchange(2, 2, Ordering::Acquire, Ordering::Relaxed)
+            {
+                Ok(..) => {
+                    return true;
+                }
+                Err(state) => match state {
+                    1 => continue,
+                    0 => break,
+                    _ => panic!("Invalid state in slot"),
+                },
+            }
+        }
+        return false;
     }
 }
