88f103a72da82ef5a83ee9bc81010d9b991c99a9
Pantae <pantae35872@gmail.com>
2026-01-03 14:50:56 +0700

    proper resize

--- build-tool/src/lib.rs
+++ build-tool/src/lib.rs
@@ -21,7 +21,7 @@ use ratatui::{
         execute,
         terminal::{EnterAlternateScreen, LeaveAlternateScreen},
     },
-    layout::{Constraint, Layout},
+    layout::{Columns, Constraint, Layout, Rect},
     prelude::CrosstermBackend,
     widgets::{Block, Paragraph},
 };
@@ -65,7 +65,7 @@ impl App {
         Self::default()
     }
 
-    fn run_command(&mut self, command: CommandBuilder) -> Result<(), Error> {
+    pub fn run_command(&mut self, command: CommandBuilder) -> Result<(), Error> {
         let (tx, rx) = channel();
         let pty_system = NativePtySystem::default();
         let pair = pty_system.openpty(Default::default()).unwrap();
@@ -84,8 +84,10 @@ impl App {
     }
 
     pub fn run(mut self, mut main_terminal: DefaultTerminal) -> Result<(), Error> {
+        let backend = CrosstermBackend::new(stdout());
         let mut repl_terminal =
-            ratatui::init_with_options(TerminalOptions { viewport: Viewport::Inline(3), ..Default::default() });
+            Terminal::with_options(backend, TerminalOptions { viewport: Viewport::Inline(3), ..Default::default() })
+                .unwrap();
         loop {
             if let Some(_status) =
                 self.child_process.as_mut().and_then(|(_name, child)| child.try_wait().ok()).flatten()
@@ -107,19 +109,27 @@ impl App {
             if !event::poll(Duration::from_millis(1)).map_err(|error| Error::Tui { error })? {
                 continue;
             }
-            if let Event::Key(key) = event::read().map_err(|error| Error::Tui { error })? {
-                match key.code {
-                    KeyCode::Char('c') if key.modifiers.contains(KeyModifiers::CONTROL) => return Ok(()),
-                    _ => {}
+            match event::read().map_err(|error| Error::Tui { error })? {
+                Event::Key(key) => {
+                    match key.code {
+                        KeyCode::Char('c') if key.modifiers.contains(KeyModifiers::CONTROL) => return Ok(()),
+                        _ => {}
+                    }
+
+                    let command = match self.promt.key_event(key) {
+                        Some(command) => command,
+                        None => continue,
+                    };
+
+                    repl::eval(&mut self, &mut main_terminal, command);
+                    repl_terminal.clear().map_err(|error| Error::Tui { error })?;
                 }
-
-                let command = match self.promt.key_event(key) {
-                    Some(command) => command,
-                    None => continue,
-                };
-
-                repl::eval(&mut self, &mut main_terminal, command);
-                repl_terminal.clear().unwrap();
+                Event::Resize(_col, row) => {
+                    main_terminal.autoresize().map_err(|error| Error::Tui { error })?;
+                    repl_terminal.autoresize().map_err(|error| Error::Tui { error })?;
+                    repl_terminal.insert_before(row, |buf| buf.reset()).map_err(|error| Error::Tui { error })?;
+                }
+                _ => {}
             }
         }
     }
