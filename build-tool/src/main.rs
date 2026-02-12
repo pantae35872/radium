39a4497ab1f445507db38486a9ba86e23ce786ad
Pantae <pantae35872@gmail.com>
2026-02-12 21:15:40 +0700

    Disable mouse capture since we don't want to implement selection ourselves

--- build-tool/src/main.rs
+++ build-tool/src/main.rs
@@ -1,12 +1,10 @@
 use std::env::args;
 
 use build_tool::App;
-use ratatui::crossterm::{self, event::EnableMouseCapture};
 
 fn main() -> Result<(), build_tool::Error> {
     let terminal = ratatui::init();
     let from_rebuild = args().nth(1);
-    crossterm::execute!(std::io::stdout(), EnableMouseCapture).unwrap();
     let app_result = App::new().run(from_rebuild, terminal);
     ratatui::restore();
     println!();
